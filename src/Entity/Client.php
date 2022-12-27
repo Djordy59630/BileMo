dae8c47f21aceb13c455be26d3dac8912b5945b0
Djordy Decroix <67747764+Djordy59630@users.noreply.github.com>
2022-12-28 00:23:26 +0100

    Company / Client / Phone entities and relations

---
+++ src/Entity/Client.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\ManyToOne(inversedBy: 'clients')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Company $company = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Phone.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhoneRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhoneRepository::class)]
+class Phone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column]
+    private ?int $storage = null;
+
+    #[ORM\Column]
+    private ?int $screenSize = null;
+
+    #[ORM\Column]
+    private ?int $pictureResolution = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $simCard = null;
+
+    #[ORM\Column]
+    private ?int $weight = null;
+
+    #[ORM\Column]
+    private ?bool $refurbished = null;
+
+    #[ORM\Column]
+    private ?int $guaranteed = null;
+
+    #[ORM\ManyToOne(inversedBy: 'phones')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Company $company = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStorage(): ?int
+    {
+        return $this->storage;
+    }
+
+    public function setStorage(int $storage): self
+    {
+        $this->storage = $storage;
+
+        return $this;
+    }
+
+    public function getScreenSize(): ?int
+    {
+        return $this->screenSize;
+    }
+
+    public function setScreenSize(int $screenSize): self
+    {
+        $this->screenSize = $screenSize;
+
+        return $this;
+    }
+
+    public function getPictureResolution(): ?int
+    {
+        return $this->pictureResolution;
+    }
+
+    public function setPictureResolution(int $pictureResolution): self
+    {
+        $this->pictureResolution = $pictureResolution;
+
+        return $this;
+    }
+
+    public function getSimCard(): ?string
+    {
+        return $this->simCard;
+    }
+
+    public function setSimCard(string $simCard): self
+    {
+        $this->simCard = $simCard;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function isRefurbished(): ?bool
+    {
+        return $this->refurbished;
+    }
+
+    public function setRefurbished(bool $refurbished): self
+    {
+        $this->refurbished = $refurbished;
+
+        return $this;
+    }
+
+    public function getGuaranteed(): ?int
+    {
+        return $this->guaranteed;
+    }
+
+    public function setGuaranteed(int $guaranteed): self
+    {
+        $this->guaranteed = $guaranteed;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+}
