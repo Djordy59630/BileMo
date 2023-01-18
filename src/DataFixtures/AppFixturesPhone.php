c33d42f7916f28f60e59e29596c13e8025aec48b
Djordy Decroix <67747764+Djordy59630@users.noreply.github.com>
2023-01-18 10:53:45 +0100

    Remake entitties & fixtures properly

--- src/DataFixtures/AppFixturesPhone.php
+++ src/DataFixtures/AppFixturesPhone.php
@@ -8,24 +8,16 @@ use App\Repository\CompanyRepository;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use App\DataFixtures\AppFixturesOrganisation;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixturesPhone extends Fixture implements DependentFixtureInterface
+class AppFixturesPhone extends Fixture 
 {
-    public function __construct(CompanyRepository $companyRespository)
-    {
-        $this->companyRespository = $companyRespository;
-    }
-
+  
     public function load(ObjectManager $manager,): void
     {
-        $companies = $this->companyRespository->findAll();
         $phoneNameList = array("Xiaomi", "Huawei", "Realme", "OPPO", "Alcatel", "OnePlus", "Google Pixel");
         $simNameList = array("Standard", "Mini SIM", "Micro SIM", "Nano SIM", "E SIM");
 
-        foreach ($companies as $company) {
-           
             for($i = 0; $i <= 9 ; $i++)
             {
                 $phoneName = array_rand($phoneNameList, 1);
@@ -40,18 +32,10 @@ class AppFixturesPhone extends Fixture implements DependentFixtureInterface
                 $phone->setWeight(mt_rand(100, 300));
                 $phone->setRefurbished(random_int(0, 1));
                 $phone->setGuaranteed(mt_rand(1, 5));
-                $phone->setCompany($company);
                 $manager->persist($phone);
                 $manager->flush();
             }
-        }
     }
 
-    public function getDependencies()
-    {
-        return [
-            AppFixturesOrganisation::class,
-        ];
-    }
 
 }
